e02b056dd54afb907c03f00421d22f3b86b1129c
Brian G. Shacklett <brian@digital-traffic.net>
2024-01-13 11:37:21 -0500

    Extract CSS into separate file

--- .eleventy.js
+++ .eleventy.js
@@ -43,6 +43,7 @@ module.exports = function (eleventyConfig) {
     "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
+    "./src/static/css/events.css": "./static/css/events.css",
   });
 
   // Copy Image Folder to /_site
