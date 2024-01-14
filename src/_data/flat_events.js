a073ebcc7824cbd139cb92aa2837c365530b9ab0
Brian G. Shacklett <brian@digital-traffic.net>
2024-01-13 19:16:15 -0500

    WIP

---
+++ src/_data/flat_events.js
@@ -0,0 +1,28 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+function loadEventsData() {
+  try {
+    const filePath = path.join(__dirname, 'events.yaml');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const data = yaml.load(fileContents);
+    let flatEvents = [];
+
+    data.forEach(hostItem => {
+      hostItem.events.forEach(event => {
+        // Optionally, add host information to each event
+        event.host = hostItem.host;
+        console.log(event);
+        flatEvents.push(event);
+      });
+    });
+
+    return flatEvents;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
+
+module.exports = loadEventsData();
