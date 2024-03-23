d7f2e620602ff74595cf352454253f6913d4551c
lastlink <theefunk@gmail.com>
2024-03-23 15:37:15 -0400

    check in netlify changes

--- src/_data/flat_events.js
+++ src/_data/flat_events.js
@@ -8,16 +8,17 @@ function loadEventsData() {
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const data = yaml.load(fileContents);
     let flatEvents = [];
-
-    data.forEach(hostItem => {
-      hostItem.events.forEach(event => {
-        // Optionally, add host information to each event
-        event.host = hostItem.host;
-        event.bannerImg = hostItem.banner_img;
-        console.log(event);
-        flatEvents.push(event);
+    if(data.eventGroups){
+      data.eventGroups.forEach(hostItem => {
+        hostItem.events.forEach(event => {
+          // Optionally, add host information to each event
+          event.host = hostItem.host;
+          event.bannerImg = hostItem.banner_img;
+          console.log(event);
+          flatEvents.push(event);
+        });
       });
-    });
+    }
 
     return flatEvents;
   } catch (e) {
